abf0d806bb8338fb27a7922b610a0c6043264b8f
Chloe Crawford <ccrawford3@zagmail.gonzaga.edu>
2020-10-02 11:51:29 -0700

    class end

--- MoreSwiftFun/main.swift
+++ MoreSwiftFun/main.swift
@@ -10,17 +10,72 @@ import Foundation
 
 print("Hello, World!")
 
-func getPositiveInteger() -> Int {
+func getPositiveInteger() throws -> Int {
     print("Enter a positive integer:")
-    if let inputString = readLine() {
-        if let inputInt = Int(inputString) {
-            if inputInt > 0 {
-                return inputInt
-            }
-        }
+    guard let inputString = readLine(), let inputInt = Int(inputString), inputInt > 0 else {
+        // if here, one of the boolean conditions failed
+        // return as part of an early exit
+        throw UserInputError.invalidPositiveInteger
     }
-    return -1
+    // if here, the boolean conditions succeeded
+    // can use input string and input int
+    return inputInt
 }
 
-let value = getPositiveInteger()
-print("got:", value)
+do{
+    let value = try getPositiveInteger()
+}
+catch {
+    print("Failed to get a positive integer")
+}
+
+enum UserInputError: Error {
+    case invalidInteger
+    case invalidPositiveInteger
+}
+
+// MARK: - Optional Chaining
+print("Enter an integer:")
+let inputStringOptional = readLine()
+
+let numDigits = inputStringOptional?.count
+print(numDigits)
+
+// MARK: - Nil Coalescing Operator ??
+if let inputString = inputStringOptional {
+    let num = Int(inputString) ?? 0
+    print(num)
+}
+
+// MARK: - Closure Practice
+
+// 1. map
+
+let nums = [1, 2, 3, 4, 5]
+
+let numsDoubled = nums.map({(value) -> Int in
+    return value * 2
+})
+print(numsDoubled)
+// shorthand
+print(nums.map{$0 * 2})
+
+// 2. filter
+
+let numsEven = nums.filter({ (value) -> Bool in
+    if value % 2 == 0 {
+        return true
+    }
+    return false
+})
+print(numsEven)
+
+//shorthand
+print(nums.filter{$0 % 2 == 0})
+
+// 3. reduce
+
+let product = nums.reduce(1, { (productSoFar, value) -> Int in
+    return productSoFar * value
+})
+print(product)
