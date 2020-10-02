370f7df2af44ee0365f0710de55117fed6e162b1
Chloe Crawford <ccrawford3@zagmail.gonzaga.edu>
2020-10-02 11:06:16 -0700

    Initial Commit

---
+++ MoreSwiftFun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MoreSwiftFun
+//
+//  Created by Crawford, Chloe on 10/2/20.
+//  Copyright © 2020 Crawford, Chloe. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
